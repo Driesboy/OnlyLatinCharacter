3242e9b04919b6e5ecfbdce3803454bf8ffbb619
Driesboy <coenendries3@gmail.com>
2018-03-12 20:40:19 +0100

    Initial commit

---
+++ src/OnlyLatinCharacters/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnlyLatinCharacters;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+
+    public function onEnable()
+    {
+        $this->getLogger()->info("§bOnly Latin Characters enabled!");
+    }
+
+
+    public function onDisable()
+    {
+        $this->getLogger()->info("§bOnly Latin Characters disabled!");
+    }
+
+    public function onPlayerChat(PlayerChatEvent $event)
+    {
+        if (!preg_match('/[^\p{Common}\p{Latin}]/u', $event->getMessage())) {
+            $event->setCancelled();
+        }
+    }
+
+}
